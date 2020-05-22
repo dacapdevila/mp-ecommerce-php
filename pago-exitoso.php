349051338737bcabee650957300928640817c333
Dario Capdevila <dariocapdevila@gmail.com>
2020-05-22 09:33:54 -0300

    Prueba de credenciales 2

--- pago-exitoso.php
+++ pago-exitoso.php
@@ -78,8 +78,7 @@
 
                     // Agrega credenciales
                     // Produccion
-                    MercadoPago\SDK::setAccessToken('TEST-436706753064257-012815-fd26983b9dc64514f89886ba9caff9fd-423390757');
-//                    MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+                    MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
                     if ( $_POST['payment_status'] == 'approved') {
                         $payment = MercadoPago\Payment::find_by_id($_POST['payment_id']);
                     }
